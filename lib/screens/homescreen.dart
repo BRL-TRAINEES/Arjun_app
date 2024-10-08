c4e2af372e2d8038fc924581171e9ec40fd826c1
Arjun Gupta <guptaarjun1711@gmail.com>
2024-10-08 22:36:08 +0530

    exitconfirmation

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -9,6 +9,27 @@ class Homescreen extends StatefulWidget {
 }
 
 class _HomescreenState extends State<Homescreen> {
+  Future<bool> _onWillPop() async {
+    return (await showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('Do you want to exit the App'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: Text('No'),
+              ),
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(true),
+                child: Text('Yes'),
+              ),
+            ],
+          ),
+        )) ??
+        false;
+  }
+
   final user = FirebaseAuth.instance.currentUser;
 
   signout() async {
@@ -17,18 +38,21 @@ class _HomescreenState extends State<Homescreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("HomeScreen"),
-      ),
-      body: Center(
-          child: Text(
-        'Welcome to Homescreen!!!',
-        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-      )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (() => signout()),
-        child: const Icon(Icons.login_rounded),
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("HomeScreen"),
+        ),
+        body: Center(
+            child: Text(
+          'Welcome to Homescreen!!!',
+          style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+        )),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (() => signout()),
+          child: const Icon(Icons.login_rounded),
+        ),
       ),
     );
   }
